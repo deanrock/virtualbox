26733795df7e3dfaeb51d51c1367e3a7c1319533
Felix Geyer <debfx-pkg@fobos.de>
2009-08-13 12:22:58 +0200

    Import 3.0.2-dfsg-1ubuntu1

---
+++ debian/virtualbox-ose.files/source_virtualbox-ose.py
@@ -0,0 +1,11 @@
+import apport.hookutils
+
+def add_info(report):
+    """Add a list of installed packages matching 'virtualbox'"""
+    report['VirtualBoxOse.DpkgList'] = apport.hookutils.command_output(["sh", "-c", "dpkg -l | grep virtualbox"])
+    
+    """Add information about installed VirtualBox kernel modules"""
+    report['VirtualBoxOse.ModInfo'] = apport.hookutils.command_output(["sh", "-c",
+        "find /lib/modules/`uname -r` -name \"vbox*\" | xargs modinfo"])
+    
+    report['LsMod'] = apport.hookutils.command_output(["lsmod"])

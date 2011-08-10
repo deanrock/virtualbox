6f88d286b4d8f4c517fce9a61c7b52bb21b34868
Felix Geyer <debfx-pkg@fobos.de>
2011-08-10 18:36:17 +0200

    Release to oneiric.

--- debian/virtualbox.files/source_virtualbox.py
+++ debian/virtualbox.files/source_virtualbox.py
@@ -2,10 +2,10 @@ import apport.hookutils
 
 def add_info(report):
     """Add a list of installed packages matching 'virtualbox' or 'linux-headers'"""
-    report['VirtualBoxOse.DpkgList'] = apport.hookutils.command_output(["sh", "-c", "dpkg -l | grep -e virtualbox -e linux-headers"])
+    report['VirtualBox.DpkgList'] = apport.hookutils.command_output(["sh", "-c", "dpkg -l | grep -e virtualbox -e linux-headers"])
 
     """Add information about installed VirtualBox kernel modules"""
-    report['VirtualBoxOse.ModInfo'] = apport.hookutils.command_output(["sh", "-c",
+    report['VirtualBox.ModInfo'] = apport.hookutils.command_output(["sh", "-c",
         "find /lib/modules/`uname -r` -name \"vbox*\" | xargs -r modinfo"])
 
     report['LsMod'] = apport.hookutils.command_output(["lsmod"])
